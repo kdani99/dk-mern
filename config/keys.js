a1ab8849c0f2a7c7550217f4125a3ff2209563d4
kdani99 <dkay@deeplearning.ai>
2018-09-23 12:43:38 -0700

    inital commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://danielle:danielle123@ds045704.mlab.com:45704/mern_dkblog'
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require(`./routes/api/items`);
+
+const app = express();
+
+//Bodyparser init
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo with Mongoose
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected ... '))
+    .catch(err => console.log(err));
+
+//use routes
+app.use(`/api/items`, items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server starated on port ${port}`))
