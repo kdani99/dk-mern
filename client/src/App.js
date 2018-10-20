35240dfa5042937b7e688c342dda77acd266953c
kdani99 <dkay@deeplearning.ai>
2018-10-20 15:05:04 -0700

    client setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import AppNavbar from './components/AppNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-class App extends Component {
+class  App extends Component {
   render() {
     return (
       <div className="App">
